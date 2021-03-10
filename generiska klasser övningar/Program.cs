4551f7e94ae046f6b53f873d53d6681b49cbf3fa
ertkonsert <martaaagren@gmail.com>
2021-03-10 10:39:40 +0100

    getting somewhere

--- generiska klasser övningar/Program.cs
+++ generiska klasser övningar/Program.cs
@@ -12,11 +12,17 @@ namespace generiska_klasser_övningar
             wares.Add("Apple", 10);
             wares.Add("Papaya", 20);
             wares.Add("Chunk o' meat", 100);
+            wares.Add("Chunk o' meat (suspicious)", 1);
 
+            Console.WriteLine("Welcome to the store! Take a look at all my beautiful wares:");
 
-            Console.WriteLine(wares["Apple"]);
+            foreach (KeyValuePair<string, int> ware in wares)
+            {
+                Console.WriteLine("{0} - {1}kr", ware.Key, ware.Value);
 
+            }
 
+            Console.WriteLine("Any you'd like to buy?");
 
             Console.ReadLine();
         }
