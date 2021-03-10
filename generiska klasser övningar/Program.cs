3c3660dc80a1ff032f6ad54a16653f414f23aa78
ertkonsert <martaaagren@gmail.com>
2021-03-10 10:25:44 +0100

    store just opened :)

---
+++ generiska klasser övningar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generiska_klasser_övningar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wares = new Dictionary<string, int>();
+
+            wares.Add("Apple", 10);
+            wares.Add("Papaya", 20);
+            wares.Add("Chunk o' meat", 100);
+
+
+            Console.WriteLine(wares["Apple"]);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
